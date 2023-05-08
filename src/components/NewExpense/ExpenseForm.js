bdbecc68e83f18f40c837a5cfcf19e75c83feb30
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2023-05-08 17:49:59 +0100

    UPDATE: two way data binding

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -56,6 +56,10 @@ const ExpenseForm = () => {
         }
 
         console.log(expenseData)
+
+        setEnteredTitle('')
+        setEnteredAmount('')
+        setEnteredDate('')
     }
 
     return (
@@ -63,15 +67,15 @@ const ExpenseForm = () => {
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type="text" onChange={titleChangeHandler} />
+                    <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+                    <input type="number" min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler} />
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type="date" min="2019-01-01" max="2022-12-31" step="0.01" onChange={dateChangeHandler} />
+                    <input type="date" min="2019-01-01" max="2022-12-31" step="0.01" value={enteredDate} onChange={dateChangeHandler} />
                 </div>
             </div>
 
